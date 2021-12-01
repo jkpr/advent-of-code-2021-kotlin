1c95648985fce682b42c84c2e6695c6c005bb182
James K. Pringle <jameskpringle@gmail.com>
2021-12-01 11:32:45 -0600

    Improvements to Day 1

--- src/day01/Main.kt
+++ src/day01/Main.kt
@@ -2,12 +2,18 @@ package day01
 
 import readInput
 
-fun part1(input: List<String>) = input.map { it.toInt() }.windowed(size=2, step=1).map { it[1] > it[0] }.count { it }
+fun part1(input: List<String>) = input.map { it.toInt() }.windowed(size=2).count { it[1] > it[0] }
 
-fun part2(input: List<String>) = input.map { it.toInt() }.windowed(size=4, step=1).map { it.takeLast(3).sum() > it.take(3).sum() }.count { it }
+fun part2(input: List<String>) = input.map { it.toInt() }.windowed(size=4).count { it.takeLast(3).sum() > it.take(3).sum() }
 
 fun main() {
     val input = readInput("day01", "input")
-    println(part1(input))
-    println(part2(input))
+    val start = System.currentTimeMillis()
+    val result1 = part1(input)
+    val result2 = part2(input)
+    println("Part 1: $result1")
+    println("Part 2: $result2")
+    val end = System.currentTimeMillis()
+    val delta = (end - start) / 1000.0
+    println("Time elapsed: $delta seconds")
 }
